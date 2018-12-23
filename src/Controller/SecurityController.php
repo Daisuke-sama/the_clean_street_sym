6340738a4c0579c3440e24a46e4851117a03d84b
Pavel Burylichau <pavel_burylichau@epam.com>
2018-12-23 18:47:31 +0300

    Added login form and styles.

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authUtils)
+    {
+        $err = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'err' => $err,
+        ]);
+    }
+}
