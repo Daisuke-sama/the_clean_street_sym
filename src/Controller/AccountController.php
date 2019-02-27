270b4a7edb0e3e92d775ae2c4feae89b59e191bc
Pavel Burylichau <paul.burilichev@gmail.com>
2019-02-27 21:19:37 +0300

    Created the endpoint to serialize object.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -30,4 +30,16 @@ class AccountController extends BaseController
 
         ]);
     }
+
+    /**
+     * @Route("/api/account", name="api_account")
+     */
+    public function accountApi()
+    {
+        $user = $this->getUser();
+
+        return $this->json($user, 200, [], [
+            'groups' => ['main'],
+        ]);
+    }
 }
