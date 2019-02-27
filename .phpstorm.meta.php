5d25a56f2c0729ffbfdd906a763f0b6b8eda2720
Pavel Burylichau <paul.burilichev@gmail.com>
2019-02-27 21:05:49 +0300

    Added files used in IDE completion.

---
+++ .phpstorm.meta.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel_Burylichau
+ * Date: 2/27/2019
+ * Time: 8:33 PM
+ */
+
+namespace PHPSTORM_META;
+
+// $container->get(Type::class) → instance of "Type"
+override(\Psr\Container\ContainerInterface::get(0), type(0));
