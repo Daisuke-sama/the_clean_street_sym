1062ef1ca73d7ab54652a3e736c8aeb918aacb9f
Pavel Burylichau <paul.burilichev@gmail.com>
2019-02-28 23:56:58 +0300

    Added custom choices for EntityType. EntityType is needed for data transformation.

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -11,6 +11,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -19,6 +20,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -36,6 +46,7 @@ class ArticleFormType extends AbstractType
                     return sprintf('(%d) %s', $user->getId(), $user->getEmail());
                 },
                 'placeholder' => 'Set an author',
+                'choices' => $this->userRepository->findAllEmailAlphabetical(),
             ]);
     }
 
